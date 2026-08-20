7d30d576fc66d8d58eecbc6394465b077c132818
wxq2027-1 <wangxueqing2025@126.com>
2026-08-20 21:50:57 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  serverExternalPackages: ['pg'],
+  serverComponentsExternalPackages: ['pg'],
 };
 
 export default nextConfig;
