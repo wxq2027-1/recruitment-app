8f973d7c2b5bc10c3587742bf54f894fc625048a
wxq2027-1 <wangxueqing2025@126.com>
2026-08-20 22:36:39 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  // 明确指定 pg 在服务端单独加载
-  serverExternalPackages: ['pg'],
-  // 可选：增大内存限制
-  experimental: {
-    serverComponentsExternalPackages: ['pg'],
-  },
 };
 
 export default nextConfig;
